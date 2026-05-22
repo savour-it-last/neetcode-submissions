d9385fe9abfd05c86edc61583279edef87896e49
HESOYAM <108790928+savour-it-last@users.noreply.github.com>
2026-05-23 01:57:46 +0530

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def check_circular_update_passed_nodes(
+        self, parent: int, node: int, edges_dict: dict[int, set[int]]
+    ) -> bool:
+        """
+        checks if when following a node either cycle or disconnectivity is present.
+        """
+        # Since its a two way connection. it goes back to itself as well.
+        if node in self.visited:
+            return False
+
+        self.visited.add(node)
+
+        for edge_end in edges_dict[node]:
+            if edge_end == parent:
+                continue
+            if not self.check_circular_update_passed_nodes(parent=node, node=edge_end, edges_dict=edges_dict):
+                return False
+
+        self.visited_all[node] = True
+        return True
+
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        edges_dict = {i: set() for i in range(n)}
+        for edge in edges:
+            edges_dict[edge[0]].add(edge[1])
+            edges_dict[edge[1]].add(edge[0])
+
+        self.visited_all = [False for i in range(n)]
+        self.visited = set()
+        if not self.check_circular_update_passed_nodes(parent=-1, node=0, edges_dict=edges_dict):
+            return False
+        if False in self.visited_all:
+            return False
+        return True
