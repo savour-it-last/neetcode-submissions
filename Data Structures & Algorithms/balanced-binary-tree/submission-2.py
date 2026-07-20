a2beccbf421c8b00503c320ff796fa7f88e4dfee
HESOYAM <108790928+savour-it-last@users.noreply.github.com>
2026-07-20 14:45:12 +0530

    Add: balanced-binary-tree - submission-2

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-2.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def _get_max_depth(self, root: Optional[TreeNode])->int:
+        if not root:
+            return 0
+        left_depth = self._get_max_depth(root=root.left)
+        if left_depth == -1:
+            return -1
+        right_depth = self._get_max_depth(root=root.right)
+        if right_depth == -1:
+            return -1
+        if abs(right_depth-left_depth) > 1:
+            return -1
+        return 1 + max(left_depth, right_depth)
+
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        
+        right_depth = self._get_max_depth(root=root.right)
+        left_depth = self._get_max_depth(root=root.left)
+        if right_depth == -1 or left_depth == -1:
+            return False
+        if abs(right_depth-left_depth) > 1:
+            return False
+        if self.isBalanced(root=root.left) and self.isBalanced(root = root.right):
+            return True
+        return False
+        
